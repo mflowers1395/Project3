c74e0599a82901b9780c070c5159831daa970f12
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-21 19:02:51 -0500

    .

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function init(){
-
-  var map;
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
-}
-}
-google.maps.event.addDomListner(window, 'load', init);
+z
