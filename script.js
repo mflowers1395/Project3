bdc8ad192aa61cbcf1bb55c2af19b71a47aa7814
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-21 20:13:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 function initMap() {
+
+  // Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+script.defer = true;
+script.async = true;
+
+// Attach your callback function to the `window` object
+window.initMap = function() {
+  // JS API is loaded and available
+};
+
+// Append the 'script' element to 'head'
+document.head.appendChild(script);
   var uluru = {lat: -25.344, lng: 131.036};
 
  var map = new google.maps.Map(document.getElementById('map'), {
