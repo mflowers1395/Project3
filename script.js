166ac84a9b0506568d164eeb879f80132b95fde9
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-21 19:59:22 -0500

    Adding map

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
-var map;
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
-  });
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
