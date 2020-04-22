65a690ffbd6ddc1315331b72bcd8a1923f1ce5a8
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-21 23:05:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.864677, lng: -87.818539},
-    zoom: 18
+    zoom: 8
   });
 }
 google.maps.event.addDomListener(window, 'load', init);
