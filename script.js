5819e03460a723f5164fcb503adf82f8d43c8789
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-28 12:54:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+  var uluru = {lat: 41.864413, lng: -87.818513};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: uluru
@@ -38,4 +38,4 @@ function initMap() {
   marker.addListener('click', function() {
     infowindow.open(map, marker);
   });
-} 
+}
