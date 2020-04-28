40f97faa16910244d3e7058ae46591ea8696cc2a
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-28 16:26:34 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,5 +1,5 @@
 var myImage=document.getElementById("mef");
-var imageArray=["Media/3rdSweater.jpg", "Media/FirstSweater.jpg", "Media/SecondDone.jpg", "Media/SecondSweater.jpg"];
+var imageArray=["Media/FirstSweater.jpg", "Media/SecondDone.jpg", "Media/SecondSweater.jpg", "Media/3rdSweater.jpg"];
 
 var imageIndex=0;
 
