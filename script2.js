b78bc9fa91bf47b2d4531c29a7be84b8c8dcc978
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-28 15:41:51 -0500

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,2 +1,14 @@
 var myImage=document.getElementById("3rd");
 var imageArray=["3rdSweater.jpg", "FirstSweater.jpg", "SecondDone.jpg", "SecondSweater.jpg"];
+
+var imageIndex=0;
+
+function changeImage (){
+  myPhoto.setAttribute("src", imageArray [ imageIndex]);
+  imageIndex++;
+  if(imageIndex>=imageArray.length){
+    imageIndex=0;
+  }
+}
+
+var intervalHandle = setInterval(changeImage,2000);
