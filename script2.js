3d70d656dbb8d8f73245ac5132580cfec49e6a2f
mflowers1395 <60019181+mflowers1395@users.noreply.github.com>
2020-04-28 15:54:03 -0500

    Adding Id

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-var myImage=document.getElementById("3rd", "2");
+var myImage=document.getElementById("3rd");
 var imageArray=["Media/3rdSweater.jpg", "Media/FirstSweater.jpg", "Media/SecondDone.jpg", "Media/SecondSweater.jpg"];
 
 var imageIndex=0;
